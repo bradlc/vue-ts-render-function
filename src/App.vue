a758e8da9f6f0037c0fab964adf0d557d37a3a78
Brad Cornes <bradlc41@gmail.com>
2021-10-08 14:44:09 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { createVNode, RenderFunction } from 'vue'
+
+const HelloWorld: RenderFunction = () => {
+  return createVNode('h1', {}, 'Hello world')
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
